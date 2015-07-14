e75d18233c5fb52ed2acba9cef8c8d4d1f0452db
Colin Stodd <cbstodd@gmail.com>
2015-07-13 22:02:03 -0400

    init commit

---
+++ scripts/ch1.js
@@ -0,0 +1,78 @@
+/**
+ * Created by colin on 7/10/15.
+ */
+
+//var theNumber = Number(prompt("How old are you?"));
+//
+//if (theNumber < 21)
+//console.log("You are not old enough to drink");
+//else if (theNumber == 21)
+//console.log("That was my favorite age");
+//else console.log("You are old enough to drink, go get drunk");
+//
+//
+
+/* while loop*/
+//var num = 0;
+//while(num <= 31){
+//    console.log(num);
+//    num = num += 1;
+//}
+
+//var result = 1;
+//var counter = 0;
+//
+//while (counter < 10){
+//    result = result * 2;
+//    counter = counter + 1;
+//}
+//console.log(result);
+
+/* Do while loop */
+//
+//do {
+//  var name = prompt("What is your name?");
+//} while (!name);
+//console.log(name);
+
+
+
+/* For loop */
+//
+//for (var number = 0; number <= 12; number += 3) {
+//    console.log(number);
+//}
+
+//var result = 1;
+//for (var counter = 0; counter < 10; counter += 1) {
+//    result = result * 2;
+//    console.log(result);
+//}
+
+/* Break Statement */
+//for (var i = 12; /*empty*/ ; i++) {
+//    if (i % 7 == 0)
+//    break;
+//}
+//console.log(i);
+
+/* Switch */
+//switch (prompt("What is the weather like?")) {
+//    case "rainy":
+//        console.log("Remember to bring an umbrella.");
+//        break;
+//    case "sunny":
+//        console.log("Dress lightly");
+//        break;
+//    case "cloudy":
+//        console.log("I like it when its cloudy some days");
+//        break;
+//    default:
+//        console.log("Well I dont know that response.");
+//        break;
+//}
+
+for (var i = 0; i <= 6; i += 1) {
+    i = "#";
+    console.log(i += "#");
+}
