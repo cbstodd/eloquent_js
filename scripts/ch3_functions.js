c5b26e24ed16510697a50973c6bc4d4b824a8fa9
Colin Stodd <cbstodd@gmail.com>
2015-07-16 22:41:52 -0400

    pausing chapter 3

--- scripts/ch3_functions.js
+++ scripts/ch3_functions.js
@@ -16,3 +16,95 @@
 //
 //console.log(power(11, 3));
 
+/* Global and local functions */
+//var o = "Outside!";
+//
+//var prac = function () {
+//    var o = "Inside the function prac";
+//};
+//
+//prac();
+//console.log(o);
+//
+//var prac2 = function () {
+//    o = "Inside the function prac2";
+//};
+//
+//prac2();
+//console.log(o);
+
+/* Landscape example */
+//var landscape = function () {
+//    result = "";
+//    var flat = function (size) {
+//        for (var i = 0; i < size; i++) {
+//            result += "_";
+//        }
+//    };
+//
+//    var mountain = function (size) {
+//        result = "//";
+//        for (var i = 0; i < size; i++) {
+//            result += "./\\\.";
+//            result += "/\\\.";
+//        }
+//    };
+//    flat(2);
+//    mountain(1);
+//
+//    return(result);
+//};
+//console.log(landscape());
+
+function hello(name) {
+    function say_name(name){
+        console.log("Hello " + name + " How are you?");
+    }
+}
+
+var colin = hello("Colin");
+console.log(colin());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
